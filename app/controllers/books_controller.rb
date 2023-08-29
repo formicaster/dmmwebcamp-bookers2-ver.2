70c6362208e23a41d2332f3251a299986e022ca6
formicaster <140141441+formicaster@users.noreply.github.com>
2023-08-29 08:56:14 +0000

    [Add] book-edit,update

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -16,7 +16,17 @@ class BooksController < ApplicationController
 
   def show
   end
-  
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
