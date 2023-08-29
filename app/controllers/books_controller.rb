42e6c72b9c227a40f2a840d621bbc9c7c9ecb17b
formicaster <140141441+formicaster@users.noreply.github.com>
2023-08-29 05:40:27 +0000

    [Add] controller-users

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,7 +2,7 @@ class BooksController < ApplicationController
   def new
     @book = Book.new
   end
-  
+
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
@@ -15,7 +15,7 @@ class BooksController < ApplicationController
 
   def show
   end
-  
+
   private
 
   def book_params
