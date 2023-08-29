98950da6e9e12c4ae16d0c35a9a17c4a358902ad
formicaster <140141441+formicaster@users.noreply.github.com>
2023-08-29 13:06:43 +0000

    [Fix] books-new

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,8 +7,11 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path
+    if @book.save
+      redirect_to books_path
+    else
+      render :new
+    end
   end
 
   def index
@@ -31,13 +34,13 @@ class BooksController < ApplicationController
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to book_path
+    redirect_to books_path
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:title, :image, :bo)
+    params.require(:book).permit(:title, :image, :body)
   end
 
   def is_matching_login_user
