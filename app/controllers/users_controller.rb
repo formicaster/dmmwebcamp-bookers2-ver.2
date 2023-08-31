a22d9e3f61f2fcec87ed3031f3391d2f02cdec08
formicaster <140141441+formicaster@users.noreply.github.com>
2023-08-31 14:43:40 +0000

    [Fix] header link

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :is_matching_login_user, only: [:edit, :update]
   def show
     @user = User.find(params[:id])
-    @books = @user.book
+    @books = @user.books
   end
 
   def edit
