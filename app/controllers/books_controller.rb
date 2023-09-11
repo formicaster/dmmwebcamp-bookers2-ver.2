536c33c9b72f9d569ff0746b380b8db740c76d79
formicaster <140141441+formicaster@users.noreply.github.com>
2023-09-11 06:25:23 +0000

    [Update] book-create redirect

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,7 +12,7 @@ class BooksController < ApplicationController
     @book.user_id = current_user.id
     if @book.save
       flash[:notice] = 'You have created book successfully.'
-      redirect_to books_path
+      redirect_to books_path(@book)
     else
       render :new
     end
