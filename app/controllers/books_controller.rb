f864ce956077fac2cb94ed55b9b81df0feb07ade
formicaster <140141441+formicaster@users.noreply.github.com>
2023-09-11 03:26:13 +0000

    [Fix]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -17,10 +17,12 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+    @user = current_user
   end
 
   def show
     @book = Book.find(params[:id])
+    @user = @book.user
   end
 
   def edit
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,12 +21,13 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
+    @user = current_user
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :profile_image)
+    params.require(:user).permit(:name, :profile_image, :introduction)
   end
 
   def is_matching_login_user
