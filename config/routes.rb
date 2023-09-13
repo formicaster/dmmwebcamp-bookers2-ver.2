020907957d43d18a7096885d2b8280b9ce6e7ec8
formicaster <140141441+formicaster@users.noreply.github.com>
2023-09-13 11:38:59 +0000

    [Add] comment controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get "/home/about" => "homes#about", as: "about"
   resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy] do
     resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create]
   end
   resources :users, only: [:show, :edit, :update, :index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
